314fd088ea4911dca5e005654669edda0227e500
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-04-25 18:44:19 -0600

    crear el proyecto y configs iniciales

---
+++ laboratorio8/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MEMORIA</h1>
+      <button>Nuevo Juego</button>
+    </div>
+  );
+}
+
+export default App;
