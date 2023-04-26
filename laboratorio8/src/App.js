be3598fc32c00918dce617b86a406f7a37e43a5f
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-04-25 19:31:02 -0600

    re-sized imgs

--- laboratorio8/src/App.js
+++ laboratorio8/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>MEMORIA</h1>
+      <h1>Memoria Formula 1</h1>
       <button>Nuevo Juego</button>
     </div>
   );
