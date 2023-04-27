d68b1a2ce8dc4030c37b9cc30ac15ec798cddade
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2023-04-27 13:14:32 -0600

    Cartas
    Poner las caras, hacerle shuffle y un poco de css para ordenarlas en grid

--- laboratorio8/src/App.js
+++ laboratorio8/src/App.js
@@ -1,12 +1,58 @@
+import { useState } from 'react';
 import './App.css';
 
+
+const cardImages = [
+  { "src": "img/AlfaRomeo.png" },
+  { "src": "img/Alpine.png" },
+  { "src": "img/AstonMartin.png" },
+  { "src": "img/F1L4.png" },
+  { "src": "img/Ferrari.png" },
+  { "src": "img/Mclaren.png" },
+  { "src": "img/Mercedes.png" },
+  { "src": "img/RedBull.png" }
+]
+
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id:Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards,turns)
+
+
   return (
     <div className="App">
       <h1>Memoria Formula 1</h1>
-      <button>Nuevo Juego</button>
+      <div class="container">
+        <button onClick={shuffleCards}>Nuevo Juego</button>
+        <div class="f1-car"></div>
+
+        <div className="card-grid"></div>
+          {cards.map(card => (
+            <div className='card' key={card.id}>
+              <div>
+                <img className='front' src={card.src} alt="card front"/>
+                <img className='back' src="/img/Cover.png" alt='card back' />
+              </div>
+            </div>
+          ))}
+
+      </div>
+     
     </div>
   );
 }
 
-export default App;
+export default App
